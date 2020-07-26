56654e0ab768ea4ab812276493681808657fe3f8
shen <15136236591@163.com>
2020-07-26 11:06:05 +0800

    项目第四次提交

--- GitTest/Git-Test/src/TestGit.java
+++ GitTest/Git-Test/src/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("项目d第一次进行修改");
+        System.out.println("项目进行了第四次提交");
     }
 }
