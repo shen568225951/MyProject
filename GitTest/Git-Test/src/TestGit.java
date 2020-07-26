3e4792cff461c591a63bb9bc52f9ec9d44cc8df1
shen <15136236591@163.com>
2020-07-26 11:34:19 +0800

    项目第八次提交

--- GitTest/Git-Test/src/TestGit.java
+++ GitTest/Git-Test/src/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("项目第七次进行修改");
+        System.out.println("第八次进行修改");
     }
 }
