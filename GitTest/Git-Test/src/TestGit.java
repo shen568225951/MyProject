d71160103055ceff250e1b177dc2f50db0adaf32
shen <15136236591@163.com>
2020-07-26 11:31:20 +0800

    项目第七次提交

--- GitTest/Git-Test/src/TestGit.java
+++ GitTest/Git-Test/src/TestGit.java
@@ -4,8 +4,6 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一次提交");
-        System.out.println("项目第一次进行修改");
-        System.out.println("项目进行了第四次提交");
-        System.out.println("这是第五次提交 进行测试 同步");
+        System.out.println("项目第七次进行修改");
     }
 }
